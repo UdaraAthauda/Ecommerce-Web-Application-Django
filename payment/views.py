7f672b5d00306aebf625b759da6d3c6aab536152
UdaraAthauda <athaudaprasath@gmail.com>
2025-05-15 11:25:17 +0530

    implementing checkout option

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from cart.cart import Cart
 
-# Create your views here.
+
+# checkout view
+def checkout(request):
+    cart = Cart(request)
+    products = cart.get_products()
+    cart_total = cart.cart_total()
+   
+    context = {'products': products, 'cart': cart, 'cart_total': cart_total}
+    
+    return render(request, 'checkout.html', context=context)
+
